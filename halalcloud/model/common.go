479b86bf8c0fb5513019d40ff1370b4ea2d0f982
zzzhr <zzzhr@hotmail.com>
2025-10-22 02:20:10 +0800

    fx int64

--- halalcloud/model/common.go
+++ halalcloud/model/common.go
@@ -2,7 +2,7 @@ package model
 
 type ScanListRequest struct {
 	Token   string         `protobuf:"bytes,1,opt,name=token,proto3" json:"token,omitempty"`
-	Limit   string         `protobuf:"varint,2,opt,name=limit,proto3" json:"limit,omitempty"`
+	Limit   int64          `protobuf:"varint,2,opt,name=limit,proto3" json:"limit,omitempty,string"`
 	OrderBy []*OrderByInfo `protobuf:"bytes,3,rep,name=order_by,json=orderBy,proto3" json:"order_by,omitempty"`
 	Version int32          `protobuf:"varint,4,opt,name=version,proto3" json:"version,omitempty"`
 }
